53c0c0f217b10b6dddbef2619c5a34a0e91d4b4d
rajasaheemkhan <rajasaheemkhan@gmail.com>
2017-10-13 15:17:39 +0530

    test

---
+++ RSKmavenProj/src/main/java/com/docker/rsk/RskHelloWorld.java
@@ -0,0 +1,10 @@
+package com.docker.rsk;
+
+public class RskHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello Docker !!!");
+	}
+
+}
